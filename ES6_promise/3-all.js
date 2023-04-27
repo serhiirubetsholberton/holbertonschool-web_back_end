271bfcda25a6502818e215236d62436db8ace268
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-27 16:34:35 -0500

    fixes

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return new Promise((res) => {
-    uploadPhoto()
-      .then(({ body }) => {
-        createUser()
-          .then(({ firstName, lastName }) => {
-            console.log(`${body} ${firstName} ${lastName}`);
-            res();
-          });
-      });
-  })
+  return uploadPhoto()
+    .then(({ body }) => {
+      createUser()
+        .then(({ firstName, lastName }) => {
+          console.log(`${body} ${firstName} ${lastName}`);
+        }).catch(() => console.log('Signup system offline'));
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
