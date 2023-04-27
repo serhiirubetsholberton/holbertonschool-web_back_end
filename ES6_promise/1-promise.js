ab901f9ab19ad9b0f3258868cb9d3b881e0fc46b
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-27 15:58:43 -0500

    add promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,14 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-  if (success) {
-    return new Promise((res, rej) => {
-      if (success) {
-        return res({
-          status: 200,
-          body: 'Success'
-        })
-      }
+  return new Promise((res, rej) => {
+    if (success) {
+      return res({
+        status: 200,
+        body: 'Success'
+      })
+    }
 
-      rej('The fake API is not working currently');
-    });
-  }
+    rej('The fake API is not working currently');
+  });
 }
