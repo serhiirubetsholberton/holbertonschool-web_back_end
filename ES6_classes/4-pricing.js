91489134aeee12d8f96ab18fdea14ea24dd9fba2
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-28 14:50:36 -0500

    added pricing class

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`
+  }
+}
