cb20e9dd2d9c2d15d10a3657bf334761a646dbf1
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-04-28 15:02:58 -0500

    added 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    console.log(this._sqft);
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
