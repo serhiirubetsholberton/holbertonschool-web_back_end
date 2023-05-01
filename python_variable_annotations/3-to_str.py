ec6cfb2dd643634b7036148afb7eec2ebe64610d
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-01 14:38:48 -0500

    to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""[summary]
+    Args:
+        n (float): Float number
+    Returns:
+        str: string representation of the float.
+    """
+
+
+def to_str(n: float) -> str:
+    """type-annotated function to_str"""
+    return str(n)
