4e6ba088af37e82b15be9d3fc8b5114ef9062e72
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-01 15:20:06 -0500

    2 added

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """measures the total execution time
+    Args:
+        n (int): spawn
+        max_delay (int): max delay
+    Returns:
+        float: returns total_time / n
+    """
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    time_process = time.perf_counter() - start_time
+    return time_process / n
