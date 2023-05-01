880a476bd14f2daddaf24125afd40e4c79a4ff2a
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-01 13:56:29 -0500

    fixed problem

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -3,9 +3,9 @@ export default function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
 
-  for (const entrie of map.entries()) {
-    if (entrie[1] === 1) {
-      entrie[1] = 100;
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      map.set(key, 100);
     }
   }
 }
