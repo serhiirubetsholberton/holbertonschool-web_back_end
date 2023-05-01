2ade1a09731f4e2f3e5ef7a0e769e13f59b8f742
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-01 11:47:28 -0500

    fixed error

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(2);
-  return new DataView(buffer).setInt8(position, value);
+  if (position >= length) throw Error('Position outside range');
+  const buff = new ArrayBuffer(length);
+  const int8 = new DataView(buff, 0);
+  int8.setInt8(position, value);
+  return int8;
 }
