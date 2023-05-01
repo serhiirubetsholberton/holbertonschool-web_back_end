8b54f2bab5765f576a4594745b7b7ffcb6647315
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-01 14:03:20 -0500

    added 11

---
+++ ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const prevCallsCount = weakMap.get(endpoint);
+
+  if (prevCallsCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  const count = prevCallsCount > 0 ? prevCallsCount + 1 : 0;
+  weakMap.set(endpoint, count);
+}
