0574adbce7d5485245063d95a2293e07f915fc93
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-01 09:31:46 -0500

    fixed 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,20 +1,22 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studentsById = students.reduce((res, student) => {
-    res[student.id] = student;
+  // const studentsById = students.reduce((res, student) => {
+  //   res[student.id] = student;
+  //   return res;
+  // }, {});
+
+  const gradesById = newGrades.reduce((res, grade) => {
+    res[grade.studentId] = grade.grade;
     return res;
   }, {});
 
-  const gradesById = newGrades.reduce((res, grade) => {
-    res[grade.id] = grade.grade;
-    return res;
-  }, {});
+  console.log(gradesById)
 
   return students
     .filter(({ location }) => location === city)
     .map((student) => {
       return {
         ...student,
-        grade: gradesById[student.id].grade
+        grade: gradesById[student.id] ?? 'N/A'
       }
     })
 }
