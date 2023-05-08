9c5aad416ce5f804ff99b1a52656dc5012bd5f11
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-08 09:02:30 -0500

    fix lint

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -4,20 +4,21 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    """Class that inherits from BaseCaching and is a caching system"""
+
     def __init__(self):
         super().__init__()
         self.data = {}
         self.next_in, self.next_out = 0, 0
 
     def _pop(self):
-        """ FIFO algorithm, remove element """
+        """FIFO algorithm, remove element"""
         self.next_out += 1
         key = self.data[self.next_out]
         del self.data[self.next_out], self.cache_data[key]
 
     def _push(self, key, item):
-        """ FIFO algorithm, add element """
+        """FIFO algorithm, add element"""
         if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
             print("DISCARD: {}".format(self.data[self.next_out + 1]))
             self._pop()
@@ -26,7 +27,7 @@ class FIFOCache(BaseCaching):
         self.data[self.next_in] = key
 
     def put(self, key, item):
-        """ Assign to the dictionary """
+        """Assign to the dictionary"""
         if key and item:
             if key in self.cache_data:
                 self.cache_data[key] = item
@@ -34,7 +35,7 @@ class FIFOCache(BaseCaching):
                 self._push(key, item)
 
     def get(self, key):
-        """ Return the value linked """
+        """Return the value linked"""
         if key is None or self.cache_data.get(key) is None:
             return None
         if key in self.cache_data:
