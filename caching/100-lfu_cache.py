c79d11a9e687a776270e0f60683baabc65b40edb
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-08 15:30:53 -0500

    added 4

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,48 +1,50 @@
 #!/usr/bin/python3
-""" MRU Caching """
+""" LFU Caching """
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
-class MRUCache(BaseCaching):
+class LFUCache(BaseCaching):
     """Class that inherits from BaseCaching and is a caching system"""
 
     def __init__(self):
         super().__init__()
-        self.head, self.tail = "head", "tail"
-        self.next, self.prev = {}, {}
-        self.handle(self.head, self.tail)
-
-    def handle(self, head, tail):
-        """MRU algorithm, handle elements"""
-        self.next[head], self.prev[tail] = tail, head
-
-    def _remove(self, key):
-        """MRU algorithm, remove element"""
-        self.handle(self.prev[key], self.next[key])
-        del self.prev[key], self.next[key], self.cache_data[key]
-
-    def _add(self, key, item):
-        """MRU algorithm, add element"""
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
-            print("DISCARD: {}".format(self.prev[self.tail]))
-            self._remove(self.prev[self.tail])
-        self.cache_data[key] = item
-        self.handle(self.prev[self.tail], key)
-        self.handle(key, self.tail)
+        self.lru_cache = OrderedDict()
+        self.lfu_cache = {}
 
     def put(self, key, item):
-        """Assign to the dictionary"""
-        if key and item:
-            if key in self.cache_data:
-                self._remove(key)
-            self._add(key, item)
+        """Assign to the dictionary, LFU algorithm"""
+        if key in self.lru_cache:
+            del self.lru_cache[key]
+        if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
+            min_value = min(self.lfu_cache.values())
+            lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
+            if len(lfu_keys) == 1:
+                print("DISCARD:", lfu_keys[0])
+                self.lru_cache.pop(lfu_keys[0])
+                del self.lfu_cache[lfu_keys[0]]
+            else:
+                for k, _ in list(self.lru_cache.items()):
+                    if k in lfu_keys:
+                        print("DISCARD:", k)
+                        self.lru_cache.pop(k)
+                        del self.lfu_cache[k]
+                        break
+        self.lru_cache[key] = item
+        self.lru_cache.move_to_end(key)
+        if key in self.lfu_cache:
+            self.lfu_cache[key] += 1
+        else:
+            self.lfu_cache[key] = 1
+        self.cache_data = dict(self.lru_cache)
 
     def get(self, key):
         """Return the value linked"""
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        if key in self.cache_data:
-            value = self.cache_data[key]
-            self._remove(key)
-            self._add(key, value)
+        if key in self.lru_cache:
+            value = self.lru_cache[key]
+            self.lru_cache.move_to_end(key)
+            if key in self.lfu_cache:
+                self.lfu_cache[key] += 1
+            else:
+                self.lfu_cache[key] = 1
             return value
