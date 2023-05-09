64d178531d0654f294e9125347936464d02bc085
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-09 08:55:19 -0500

    added get_hyper

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -50,11 +50,11 @@ class Server:
         assert type(page) == int and page > 0
         assert type(page_size) == int and page_size > 0
         total_pages = math.floor(len(self.dataset()) / page_size)
-        data = self.get_page(page, page_size)
+
         return {
-            "page_size": len(data),
+            "page_size": len(self.get_page(page, page_size)),
             "page": page,
-            "data": data,
+            "data": self.get_page(page, page_size),
             "next_page": page + 1 if page + 1 < total_pages else None,
             "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages,
