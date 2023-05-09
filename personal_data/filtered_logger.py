5c3c37ac61f234a9572254c4bb923889f6e14e0b
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-09 14:41:07 -0500

    added 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ Filtered logger module
 import re
 import logging
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -19,3 +20,23 @@ def filter_datum(
             message,
         )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = (
+        "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    )
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
