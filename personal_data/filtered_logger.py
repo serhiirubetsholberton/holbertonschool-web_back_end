9479b5b70c18d5b7c8ad740ded29ba29f54a73c9
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-09 15:05:08 -0500

    added get_logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ import logging
 from typing import List
 import logging
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
@@ -22,6 +22,20 @@ def filter_datum(
             message,
         )
     return message
+
+
+def get_logger() -> logging.Logger:
+    """Description: Implement a get_logger function that takes no arguments and returns a logging.Logger object."""
+    log = logging.getLogger("user_data")
+    log.setLevel(logging.INFO)
+    log.propagate = False
+
+    sh = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    sh.setFormatter(formatter)
+    log.addHandler(sh)
+
+    return log
 
 
 class RedactingFormatter(logging.Formatter):
@@ -38,21 +52,10 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Filters values in incoming log records using filter_datum """
-        return filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
-                            self.SEPARATOR)
-
-    def get_logger() -> logging.Logger:
-        ''' Description: Implement a get_logger function that takes no arguments and returns a logging.Logger object.
-        '''
-        log = logging.getLogger('user_data')
-        log.setLevel(logging.INFO)
-        log.propagate = False
-
-        sh = logging.StreamHandler()
-        formatter = RedactingFormatter(PII_FIELDS)
-        sh.setFormatter(formatter)
-        log.addHandler(sh)
-
-        return log
+        """Filters values in incoming log records using filter_datum"""
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            super(RedactingFormatter, self).format(record),
+            self.SEPARATOR,
+        )
