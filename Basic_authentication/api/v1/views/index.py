e7837aa90de52cfec2b939f6ed4463741e69fe46
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-10 15:41:01 -0500

    added 403

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,11 @@ def unauthorized() -> str:
       - raises a 401 error by using abort
     """
     abort(401)
+
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
+def unauthorized() -> str:
+    """GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
+    """
+    abort(403)
