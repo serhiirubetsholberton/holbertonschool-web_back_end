8ed1ada2d469b33d6d57cf7f017ca9b32a473fa5
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-10 16:19:50 -0500

    added basic auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module of Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
+
+
+class BasicAuth(Auth):
+    """Basic Authentication Class"""
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """Extract Base 64 Authorization Header"""
+
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        encoded = authorization_header.split(" ", 1)[1]
+
+        return encoded
