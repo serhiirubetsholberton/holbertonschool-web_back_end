e2f506a1d2f90c4266d885b7cd294318adc7a7fa
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-11 16:19:27 -0500

    added auth_session/login

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -42,12 +42,15 @@ def before_request() -> str:
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/",
     ]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None or auth.session_cookie(
+        request
+    ):
         abort(401)
 
     if auth.current_user(request) is None:
