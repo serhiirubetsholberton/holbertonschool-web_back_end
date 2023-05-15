3eb63c1ebfca4f342b90b0c93862d24b039c2b78
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-15 15:26:59 -0500

    added before request 5 task

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -47,12 +47,9 @@ def before_request() -> str:
 
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if auth.authorization_header(request) is None or auth.session_cookie(
-        request
-    ):
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
         abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
