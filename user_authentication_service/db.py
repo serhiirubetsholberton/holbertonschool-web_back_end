40486afca6c69faa8660557b3999ce753f49d1b3
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-18 16:26:13 -0500

    added bash comment

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,5 +1,6 @@
-"""DB module
-"""
+#!/usr/bin/env python3
+""" Database for ORM """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
