165a1199496c07f878b8f013155998a8517cb280
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-19 11:26:05 -0500

    added get user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
 from typing import TypeVar
 from user import Base, User
 
