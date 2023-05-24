ab600f7195ccfb4264708b6ec8f9da5c411494d3
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-24 09:06:35 -0500

    Added 4

---
+++ i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Basic Flask app basic babel setup, locale from request,
+    parametrize templates, force locale with url parameter """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    """ Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale from request """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def root():
+    """ Root route """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
